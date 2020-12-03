593402991c6a24425c3b2c668ac4a6aa53d64063
Moisés Muñiz Mangas <36801152+moemmia@users.noreply.github.com>
2020-12-03 12:55:52 +0100

    Main Menu

---
+++ Assets/Scripts/UI/SceneLoader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    public KeyCode key;
+    public string sceneName;
+    void Update() {
+        if(Input.GetKeyDown(key)) {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
